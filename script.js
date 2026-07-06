adb45e95bddb25663ced97abdf2907bfe06053fc
sagittariussky8081-sys <sagittarius.sky8081@gmail.com>
2026-07-07 04:45:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -15,4 +15,16 @@ function nextFact() {
 }
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
 }
+
+window.onload = function () {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+};
